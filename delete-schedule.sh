ee5e84224ed62a54a71e4a189e0451894191f1dd
aim-leo <luozewei123@qq.com>
2020-06-10 15:45:34 +0800

    feat: 🎸 enable set cron at add-schedule, enable delete schedule

--- delete-schedule.sh
+++ delete-schedule.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/bash
 echo=$(which echo)
-bash=$(which bash)
-pwd=$(which pwd)
 test=$(which test)
 sed=$(which sed)
 tail=$(which tail)
 readlink=$(which readlink)
 
-current=$($pwd)
-whoami=$(/usr/bin/whoami)
 target=/etc/crontab
 
 # User should be root
@@ -19,7 +15,7 @@ fi
 
 if $test ! -f "$1"; then
   $echo "Yaml file unexsist!"
-  $echo "Usage: bash add-schedule.sh [yaml] [cron_set]"
+  $echo "Usage: bash delete-schedule.sh [yaml]"
   exit 1
 fi
 
@@ -27,22 +23,22 @@ YAML_PATH=`$readlink -f $1`
 
 ADDED_FLAG=`$tail $target | grep $1`
 
-# run it at each hours, 00:05 01:05 ...
-CRON_SET="5 * * * *"
-# if you have determine at shell
-if [[ ! -z "$2" ]]; then
-  CRON_SET=$2
-fi
-
 if [[ ! -z "${ADDED_FLAG}" ]]; then
-  $echo "you have added $1 to /etc/crontab, replacing"
+  $echo "Deleting schedule at $1"
   $sed -i "/$1/d" $target
+  if [ $? -ne 0 ]; then
+    $echo "Deleting schedule $1 fail!"
+    exit 1
+  fi
+else
+  $echo "Deleting schedule $1 fail! this schedule is unregister!"
+  exit 1
 fi
-
-$echo "${CRON_SET} ${whoami} ${bash} ${current}/db-backup.sh -f $YAML_PATH" >> $target || exit 1
 
 cat /etc/crontab
 
 service cron reload
+
+$echo "Deleting schedule $1 success!"
 
 exit 0
